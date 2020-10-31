08ff6a9320bd2776e6fc076789e9d1e41f574740
m-zaink <sidsmith9@gmail.com>
2020-11-01 00:00:10 +0530

    Add functionality to buttons on HomeScreen

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,10 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var eggBoilingProgressView: UIProgressView!
+    
+    
+    let eggBoilingDurationsBasedOnHardness: [String: Int] = [
+        "Soft": 5,
+        "Medium": 7,
+        "Hard": 12
+    ]
+    
+    var eggBoilingTimer: Timer?
+    var audioPlayer: AVAudioPlayer?
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        print("Connected to all three buttons")
+        guard let currentTitle = sender.currentTitle, let eggBoilingTimeInMinutes = eggBoilingDurationsBasedOnHardness[currentTitle] else {
+            return
+        }
+        
+        initiateEggBoilingTimer(withEggBoilingDurationInSeconds: eggBoilingTimeInMinutes)
+    }
+    
+    func initiateEggBoilingTimer(withEggBoilingDurationInSeconds eggBoilingDurationInSeconds: Int) {
+        titleLabel.text = "Egg is boiling! Please be patient"
+        
+        eggBoilingTimer?.invalidate()
+        
+        eggBoilingProgressView.progress = 0.0
+        
+        var secondsElapsedSinceEggBoilingTimerStarted = 0
+        
+        eggBoilingTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+            secondsElapsedSinceEggBoilingTimerStarted += 1
+            
+            self.eggBoilingProgressView.progress = Float(secondsElapsedSinceEggBoilingTimerStarted) / Float(eggBoilingDurationInSeconds)
+            
+            
+            if secondsElapsedSinceEggBoilingTimerStarted == eggBoilingDurationInSeconds {
+                timer.invalidate()
+                
+                self.titleLabel.text = "Tada! Egg is boiled"
+                
+                if let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3"), let player = try? AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue) {
+                    self.audioPlayer = player
+                    player.play()
+                }
+            }
+        }
     }
 }
